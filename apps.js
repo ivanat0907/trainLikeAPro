9b9aac67d7d680c704f6dcbca5d67adf906c45b2
Ivana Topalovic <122395297+ivanat0907@users.noreply.github.com>
2023-01-20 20:27:09 +0100

    Add files via upload

--- apps.js
+++ apps.js
@@ -3,7 +3,7 @@ const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
 //Display Mobile Menu
-const mobileMenu = () = {
+function mobileMenu() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 };
@@ -11,32 +11,33 @@ const mobileMenu = () = {
 menu.addEventListener('click', mobileMenu);
 
 //Show active menu when scrolling
-const highlightMenu = () = {
+function highlightMenu(){
     const elem = document.querySelector('.highlight');
     const homeMenu = document.querySelector('#home-page');
     const aboutMenu = document.querySelector('#about-page');
     const servicesMenu = document.querySelector('#services-page');
     let scrollPos = window.scrollY;
-    console.log(scrollPos);
+    //console.log(scrollPos);
+    //console.log(window.innerWidth);
 
     // adds 'highlight'
     // class to my menu items
-    if (window.innerWidth 960 && scrollPos 600) {
+    if (window.innerWidth  == 960 && scrollPos == 600) {
         homeMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         return;
-    } else if (window.innerWidth 960 && scrollPos 1400) {
+    } else if (window.innerWidth ==960 && scrollPos ==1400) {
         aboutMenu.classList.add('highlight');
         homeMenu.classList.remove('highlight');
         servicesMenu.classList.remove('highlight');
         return;
-    } else if (window.innerWidth 960 && scrollPos 2345) {
+    } else if (window.innerWidth ==960 && scrollPos== 2345) {
         servicesMenu.classList.add('highlight');
         aboutMenu.classList.remove('highlight');
         return;
     }
 
-    if ((elem && window.innerWIdth 960 && scrollPos 600) elem) {
+    if ((elem && window.innerWIdth <960 && scrollPos<600) || elem) {
         elem.classList.remove('highlight');
     }
 };
@@ -45,9 +46,9 @@ window.addEventListener('scroll', highlightMenu);
 window.addEventListener('click', highlightMenu);
 
 //Close mobile Menu when clicking on a menu item
-const hideMobileMenu = () = {
+function hideMobileMenu() {
     const menuBars = document.querySelector('.is-active');
-    if (window.innerWidth = 768 && menuBars) {
+    if (window.innerWidth <= 768 && menuBars) {
         menu.classList.toggle('is-active');
         menuLinks.classList.remove('active');
     }
